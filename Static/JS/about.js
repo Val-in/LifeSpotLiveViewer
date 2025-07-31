f9010b7718210900e16437d431df8c669120e1fa
Valeriia <valeriia.vp1@yandex.ru>
2025-07-31 16:19:23 +0300

    Added slider

--- Static/JS/about.js
+++ Static/JS/about.js
@@ -17,8 +17,33 @@ function getReview() {
     // Сохраним текущее время
     review["date"] = new Date().toLocaleString()
 
-    // Добавим на страницу
-    writeReview(review)
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if(enableLikes) {
+        review.rate = 0;
+    } 
+        writeReview(review);
+}
+
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] =  resultNum;
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
 }
 
 /*
@@ -26,14 +51,23 @@ function getReview() {
 * 
 * */
 const writeReview = (review) => {
+      
     const reviewsContainer = document.getElementById("reviews");
     const reviewDivision = document.createElement("div");
     reviewDivision.className = "review-text";
-    
+
+    let likeInfo = '';
+    if (review.hasOwnProperty('rate')) {
+        let commentId = Math.random();
+        
+        likeInfo += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`;
+    }
+
     reviewDivision.innerHTML = `
-     <p><i><b>${review.userName}</b>  ${review.date}</i></p>
+        <p><i><b>${review.userName}</b> ${review.date} ${likeInfo}</i></p>
         <p>${review.comment}</p>
     `;
+
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "X";
--- Static/JS/index.js
+++ Static/JS/index.js
@@ -1,52 +1,76 @@
-let sessionHandler = function handleSession() {
+function handleSession (logger, checker){
 
-    // создадим объект Map для хранения сессии
-    let session = new Map();
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
-
-    let age = Number(prompt("Пожалуйста, введите ваш возраст"));
-    session.set("age", age);
-
-    if (age >= 18) {
-        let startDate = new Date().toLocaleString();
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
-
-        setTimeout(() =>
-                alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
-            30000);
+    // Проверяем дату захода и проставляем, если новый визит
+    if(window.sessionStorage.getItem("startDate") == null){
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
     }
-    else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        setTimeout(() => window.location.href = "http://www.google.com", 2000); 
+
+    // Проверяем userAgent и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAgent") == null){
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
     }
-    // Вывод в консоль
-    for (let result of session) {
-        console.log(result)
+
+    // Проверяем возраст и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAge") == null){
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    }else{
+
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
     }
+    
+    logger()
 }
 
-sessionHandler();
+    let checker = function (newVisit) {
+        if (window.sessionStorage.getItem("userAge") >= 18) {
+            if (newVisit) {
+                alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
 
-let contentFilter = function filterContent() {
-
-    // Сохраняем текст пользовательского запроса.
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
-    // Находим контейнеры с видео, которые необходимо фильтровать
-    let elements = document.getElementsByClassName('video-container');
-
-    // Пробегаемся по контейнерам
-    for (let i = 0; i <= elements.length; i++) {
-        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
-        let videoText = elements[i].querySelector(".video-title").innerText;
-        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
-            // Скрываем неподходящие
-            elements[i].style.display = 'none';
-        } else {
-            // Показываем подходящие
-            elements[i].style.display = 'inline-block';
+                setTimeout(() =>
+                        alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
+                    30000);
+            }
+        } 
+        else {
+            alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+            setTimeout(() => window.location.href = "http://www.google.com", 2000);
         }
     }
-}
+    // Вывод в консоль
+    let logger = function () {
+        console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+        console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+        console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
+    }
+
+    window.addEventListener('DOMContentLoaded', () => {
+        // Этот код выполнится, когда HTML полностью загружен
+        handleSession(logger, checker);
+
+    let contentFilter = function filterContent() {
+    
+        // Сохраняем текст пользовательского запроса.
+        let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+        // Находим контейнеры с видео, которые необходимо фильтровать
+        let elements = document.getElementsByClassName('video-container');
+    
+        // Пробегаемся по контейнерам
+        for (let i = 0; i < elements.length; i++) {
+            // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+            let videoText = elements[i].querySelector(".video-title").innerText;
+            // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+            if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+                // Скрываем неподходящие
+                elements[i].style.display = 'none';
+            } else {
+                // Показываем подходящие
+                elements[i].style.display = 'inline-block';
+            }
+        }
+    }
